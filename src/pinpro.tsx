2b2280ebf2d3fd68b8e569ba5f0ebdbca1b2dbd8
Bartam0us <117825208+Bartamous@users.noreply.github.com>
2026-02-13 22:21:14 +1100

    tauri added run npm run tauri:dev or tauri:build

--- src/pinpro.tsx
+++ src/pinpro.tsx
@@ -2,7 +2,11 @@ import axios from 'axios';
 
 const apiUrl = 'http://pinpro/';
 
-async function getUser(id: number): Promise<User> {
-  const response = await axios.get<User>(`${apiUrl}/users/${id}`);
+async function getPinpro() {
+  const response = await axios.get(apiUrl);
   return response.data;
 }
+
+getPinpro().then((data) => {
+  console.log(data);
+});
